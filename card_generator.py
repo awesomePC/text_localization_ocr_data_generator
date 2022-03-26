deb3ee145d6f4d365e885d40b52527c4c55d3ea0
machinePC <billycrow268@gmail.com>
2022-03-26 15:58:45 +0530

    modified card renderer script to render first box

--- card_generator.py
+++ card_generator.py
@@ -3,6 +3,9 @@ import subprocess
 import imutils
 from PIL import Image
 
+# importing the module
+import json
+
 parent_folder = os.path.dirname(os.path.abspath(__file__))
 submodule_path = os.path.join(parent_folder, "TextRecognitionDataGenerator")
 sys.path.append(submodule_path)
@@ -23,8 +26,23 @@ common_cmd_params = [
 ]
 
 def resize_pil_image(img, width=None, height=None):
+    """
+    Resize PIL image - maintain aspect ratio
+
+    Args:
+        img (PIL): Pil image to resize
+        width (int, optional): If resize by width. Defaults to None.
+        height (int, optional): If resizing by height. Defaults to None.
+
+    Returns:
+        PIL: Resized image
+    """
     dim = None
     (w, h) = img.size
+
+    if not height and not width:
+        print(f"Error ... Either new width or height should be passed..")
+        return False
 
     # check to see if the width is None
     if width is None:
@@ -43,26 +61,93 @@ def resize_pil_image(img, width=None, height=None):
     # resize the image
     return img.resize(dim, Image.ANTIALIAS)
 
+def get_points_dims(points):
+    """
+    Get points height and width dimensions
+
+    Args:
+        points (list): List of list -- 4 points format
+    """
+    # print(f"transcription: ", transcription)
+    # print(f"points: ", points)
+    height = max(
+        points[3][1] - points[0][1],
+        points[2][1] - points[1][1],
+    )
+    ## by x point difference
+    width = max(
+        points[1][0] - points[0][0],
+        points[2][0] - points[3][0],
+    )
+    # print(f"height: ", height)
+    # print(f"width: ", width)
+    return (height, width)
 
 def main():
     """
         Description: Main function
     """
+    ## ---------------------------------------------------------------------
+    # Opening JSON file
+    json_meta_file = "./data/document-id-template/UAE-identity-card-front/meta.json"
+    with open(json_meta_file) as json_file:
+        meta_data = json.load(json_file)
+    
+    # print(meta_data)
+
+    project_name = meta_data["project_name"]
+    base_dir_path = meta_data["base_dir_path"]
+    image_path_original = meta_data["image_path"]["original"]
+    blank_image_path = meta_data["image_path"]["blank_image"]
+    boxes = meta_data["boxes"]
+
+    ## --------------------------------------------------------------------
+    base_dir = base_dir_path # "./data/document-id-template/UAE-identity-card-front"
     ## Pasting generated word image
-    doc_cleaned_img_file = "./data/document-id-template/UAE-identity-card-front/cleaned.png"
+    doc_cleaned_img_file = os.path.join(base_dir, blank_image_path) # "cleaned.png")
     document_bg_img = Image.open(doc_cleaned_img_file).convert("RGBA")
 
-    ## ----------------------------------------------------------------------
-    output_dir = "out"
-    height = 26
+    # ## ----------------------------------------------------------------------
+    output_root_dir = os.path.join("./out", project_name)
+    os.makedirs(output_root_dir, exist_ok=True)
+
+    ## Testing first block -- rendering
+    box_index = 0
+    box = boxes[box_index]
+
+    box_type = box["box_type"]
+    box_coordinates = box["box_coordinates"]
+    alignment = box["alignment"]
+    render_text = box["render_text"]
+    language = box["lang"]
+    is_multi_lang_subset = box["text"]
+    font = box["font"]
+    dict_file = box["dict_file"]
+    dict_path = os.path.join(base_dir, dict_file)
+
+    ###
+    height = font["font_size"] # 26
+    margins = 3
+    dest_dir = os.path.join(output_root_dir, f"box_{box_index}")
+    box_height, box_width = get_points_dims(points=box_coordinates)
+    font_name = font["name"]
+
+    if language in ["en", "english"]:
+        font_parent = "latin"
+    elif language in ["ar", "arabic"]:
+        font_parent = "ar"
+
+    font_filepath = f"TextRecognitionDataGenerator/trdg/fonts/{font_parent}/{font_name}.ttf"
+    if not os.path.exists(font_filepath):
+        print(f"Error .. Font filepath: {font_filepath} not exists on disk ..")
 
     cmd = [
-        "--dict", "./data/document-id-template/UAE-identity-card-front/dicts/text-2--الإمارات العربية المتحدة.txt",
-        "--language", "ar",
-        "--margins", "3",
+        "--dict", dict_path, # "./data/document-id-template/UAE-identity-card-front/dicts/text-2--الإمارات العربية المتحدة.txt",
+        "--language", language, # "ar",
+        "--margins", str(margins),
         "--format", str(height),
-        "--font", "TextRecognitionDataGenerator/trdg/fonts/ar/Times-New-Roman.ttf",
-        "--output_dir", output_dir,
+        "--font", font_filepath, # "TextRecognitionDataGenerator/trdg/fonts/ar/Times-New-Roman.ttf",
+        "--output_dir", dest_dir,
     ]
 
     ## final command
@@ -73,18 +158,18 @@ def main():
     # print(result)
     ## ----------------------------------------------------------------------
 
-    # word_image_file = os.path.join(output_dir, f"United Arab Emirates_0.png")
-    # word_image_file = os.path.join(output_dir, f"Identity Card_0.png")
-    # word_image_file = os.path.join(output_dir, f"ﺓﺪﺤﺘﻤﻟﺍ ﺔﻴﺑﺮﻌﻟﺍ ﺕﺍﺭﺎﻣﻹﺍ ﺔﻟﻭﺩ_0.png")
-    word_image_file = os.path.join(output_dir, f"0.png")
+    # # word_image_file = os.path.join(output_dir, f"United Arab Emirates_0.png")
+    # # word_image_file = os.path.join(output_dir, f"Identity Card_0.png")
+    # # word_image_file = os.path.join(output_dir, f"ﺓﺪﺤﺘﻤﻟﺍ ﺔﻴﺑﺮﻌﻟﺍ ﺕﺍﺭﺎﻣﻹﺍ ﺔﻟﻭﺩ_0.png")
+    word_image_file = os.path.join(dest_dir, f"0.png")
     word_img = Image.open(word_image_file).convert("RGBA")
-    word_img = resize_pil_image(word_img, height=22)
+    word_img = resize_pil_image(word_img, height=box_height)
 
-    # new_position_x, new_position_y = 14, 10
-    # new_position_x, new_position_y = 55, 42
-    new_position_x, new_position_y = 245, 7
+    # # new_position_x, new_position_y = 14, 10
+    # # new_position_x, new_position_y = 55, 42
+    new_position_x, new_position_y = box_coordinates[0] # 245, 7
     document_bg_img.paste(word_img,(new_position_x, new_position_y), mask=word_img)
-    out_filepath = os.path.join(output_dir, f"result_doc.png")
+    out_filepath = os.path.join(output_root_dir, f"result_rendered.png")
     document_bg_img.save(out_filepath)
 
 if __name__ == "__main__":
